0a18fd3299107cc3df2538a0c5b8a866c5b4e533
nannan829 <3988529910@qq.com>
2026-07-09 22:57:06 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,456 @@
+    /*
+      明信片图片数据区
+      GitHub版：请上传 index.html 和整个 images 文件夹。
+      - 压缩包版：图片路径为 images/1.jpg、images/2.jpg ……
+      - 如果你替换图片，请保持文件名一致，或修改下方 image 字段。
+      - 如果要添加更多明信片，复制一个对象继续添加即可。
+    */
+    const postcards = [
+          {
+                    "image": "images/1.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 1
+          },
+          {
+                    "image": "images/2.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 2
+          },
+          {
+                    "image": "images/3.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 3
+          },
+          {
+                    "image": "images/4.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 4
+          },
+          {
+                    "image": "images/5.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 5
+          },
+          {
+                    "image": "images/6.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 6
+          },
+          {
+                    "image": "images/7.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 7
+          },
+          {
+                    "image": "images/8.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 8
+          },
+          {
+                    "image": "images/9.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 9
+          },
+          {
+                    "image": "images/10.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 10
+          },
+          {
+                    "image": "images/11.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 11
+          },
+          {
+                    "image": "images/12.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 12
+          },
+          {
+                    "image": "images/13.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 13
+          },
+          {
+                    "image": "images/14.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 14
+          },
+          {
+                    "image": "images/15.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 15
+          },
+          {
+                    "image": "images/16.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 16
+          },
+          {
+                    "image": "images/17.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 17
+          },
+          {
+                    "image": "images/18.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 18
+          },
+          {
+                    "image": "images/19.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 19
+          },
+          {
+                    "image": "images/20.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 20
+          },
+          {
+                    "image": "images/21.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 21
+          },
+          {
+                    "image": "images/22.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 22
+          },
+          {
+                    "image": "images/23.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 23
+          },
+          {
+                    "image": "images/24.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 24
+          },
+          {
+                    "image": "images/26.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 26
+          },
+          {
+                    "image": "images/27.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 27
+          },
+          {
+                    "image": "images/28.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 28
+          },
+          {
+                    "image": "images/29.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 29
+          },
+          {
+                    "image": "images/30.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 30
+          },
+          {
+                    "image": "images/31.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 31
+          },
+          {
+                    "image": "images/32.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 32
+          },
+          {
+                    "image": "images/33.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 33
+          },
+          {
+                    "image": "images/34.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 34
+          },
+          {
+                    "image": "images/35.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 35
+          },
+          {
+                    "image": "images/36.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 36
+          },
+          {
+                    "image": "images/37.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 37
+          },
+          {
+                    "image": "images/38.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 38
+          },
+          {
+                    "image": "images/39.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 39
+          },
+          {
+                    "image": "images/40.jpg",
+                    "from": "育英学校的小朋友",
+                    "to": "儿童医院的小朋友",
+                    "message": "愿你的每一天，都有阳光和笑容。",
+                    "date": "2025.10.6",
+                    "stamp": "LOVE",
+                    "number": 40
+          }
+];
+
+    const stage = document.getElementById('stage');
+    const modal = document.getElementById('modal');
+    const galleryGrid = document.getElementById('galleryGrid');
+    const btnViewAll = document.getElementById('btnViewAll');
+    const btnClose = document.getElementById('btnClose');
+    const pathTip = document.getElementById('pathTip');
+
+    let currentIndex = 0;
+    let timer = null;
+    const intervalMs = 2800; // 在这里调整明信片出现间隔，数值越小出现越快
+
+    function createImageBlock(data) {
+      const wrap = document.createElement('div');
+      wrap.className = 'postcard-img-container';
+      wrap.innerHTML = `
+        <div class="placeholder"><div class="icon">✉</div><div>图片未加载，请检查 images 文件夹</div></div>
+        <img src="${data.image}" alt="儿童祝福明信片 ${data.number || ''}" loading="eager">
+      `;
+      const img = wrap.querySelector('img');
+      img.addEventListener('error', () => {
+        img.style.display = 'none';
+        pathTip.style.display = 'block';
+      });
+      img.addEventListener('load', () => {
+        img.style.display = 'block';
+        // 自动判断横版/竖版：竖版明信片完整显示，不再被裁切
+        wrap.classList.remove('portrait-image', 'landscape-image');
+        if (img.naturalHeight > img.naturalWidth * 1.08) {
+          wrap.classList.add('portrait-image');
+        } else {
+          wrap.classList.add('landscape-image');
+        }
+      });
+      return wrap;
+    }
+
+    function createPostcard(data, gallery = false) {
+      const card = document.createElement('article');
+      card.className = gallery ? 'gallery-card' : 'postcard';
+      const rot = ((Math.random() * 6) - 3).toFixed(2) + 'deg';
+      card.style.setProperty('--rot', rot);
+      card.appendChild(createImageBlock(data));
+
+      const caption = document.createElement('div');
+      caption.className = 'caption-line';
+      caption.innerHTML = `<span>FROM：${data.from || '孩子们'}</span><span class="stamp">${data.stamp || 'LOVE'}</span>`;
+      card.appendChild(caption);
+      return card;
+    }
+
+    function showNextPostcard() {
+      if (!postcards.length) return;
+      const data = postcards[currentIndex];
+      const card = createPostcard(data, false);
+      stage.appendChild(card);
+      setTimeout(() => {
+        if (card && card.parentNode) card.parentNode.removeChild(card);
+      }, 6700);
+      currentIndex = (currentIndex + 1) % postcards.length;
+    }
+
+    function startLoop() {
+      stopLoop();
+      showNextPostcard();
+      timer = setInterval(showNextPostcard, intervalMs);
+    }
+
+    function stopLoop() {
+      if (timer) clearInterval(timer);
+      timer = null;
+    }
+
+    function openGallery() {
+      stopLoop();
+      galleryGrid.innerHTML = '';
+      postcards.forEach((data) => galleryGrid.appendChild(createPostcard(data, true)));
+      modal.classList.add('active');
+    }
+
+    function closeGallery() {
+      modal.classList.remove('active');
+      startLoop();
+    }
+
+    btnViewAll.addEventListener('click', openGallery);
+    btnClose.addEventListener('click', closeGallery);
+    modal.addEventListener('click', (event) => {
+      if (event.target === modal) closeGallery();
+    });
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape' && modal.classList.contains('active')) closeGallery();
+    });
+
+    document.addEventListener('DOMContentLoaded', startLoop);
+  
